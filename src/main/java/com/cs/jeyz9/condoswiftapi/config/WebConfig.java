cef659923a9a4087e5753045932b290ebefe7a7b
JeyZ9 <ggjj4511@gmail.com>
2025-10-14 21:13:18 +0700

    DEBUG CORS

--- src/main/java/com/cs/jeyz9/condoswiftapi/config/WebConfig.java
+++ src/main/java/com/cs/jeyz9/condoswiftapi/config/WebConfig.java
@@ -25,8 +25,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Authorization");
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization");
     }
     
     @Bean
