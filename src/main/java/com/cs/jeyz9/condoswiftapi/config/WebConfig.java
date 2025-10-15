740c0dd0e0db19f8c7f67c22b020ba046d532be7
JeyZ9 <ggjj4511@gmail.com>
2025-10-15 12:36:44 +0700

    test path get image

--- src/main/java/com/cs/jeyz9/condoswiftapi/config/WebConfig.java
+++ src/main/java/com/cs/jeyz9/condoswiftapi/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:./uploads/")
+                .addResourceLocations("file:/uploads/")
 //                .addResourceLocations("file:./app/uploads/")
                 .setCachePeriod(0);
     }
