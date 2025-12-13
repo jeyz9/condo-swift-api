8ec5410ab399f540fb9671c8089e8326e1b9fe55
JeyZ9 <ggjj4511@gmail.com>
2025-12-13 20:10:00 +0700

    debug require new otp

--- src/main/java/com/cs/jeyz9/condoswiftapi/repository/VerificationOtpTokenRepository.java
+++ src/main/java/com/cs/jeyz9/condoswiftapi/repository/VerificationOtpTokenRepository.java
@@ -2,8 +2,13 @@ package com.cs.jeyz9.condoswiftapi.repository;
 
 import com.cs.jeyz9.condoswiftapi.models.VerificationOtpToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +16,11 @@ public interface VerificationOtpTokenRepository extends JpaRepository<Verificati
     Optional<VerificationOtpToken> findByToken(String token);
     
     Optional<VerificationOtpToken> findByUserId(Long userId);
+
+    @Modifying
+    @Transactional
+    @Query(value = """
+        DELETE FROM VerificationOtpToken WHERE expiredAt < :now
+    """)
+    void deleteAllExpired(@Param("now") LocalDateTime now);
 }
