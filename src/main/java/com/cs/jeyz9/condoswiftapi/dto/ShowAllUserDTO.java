e50ff0b11fcd8e9ff1433edcc7d49d44c8cdcbca
JeyZ9 <ggjj4511@gmail.com>
2026-01-04 21:33:15 +0700

    show all user for admin

--- src/main/java/com/cs/jeyz9/condoswiftapi/dto/ShowAllUserDTO.java
+++ src/main/java/com/cs/jeyz9/condoswiftapi/dto/ShowAllUserDTO.java
@@ -1,7 +1,23 @@
 package com.cs.jeyz9.condoswiftapi.dto;
 
-public interface ShowAllUserDTO {
-    Long getId();
-    String getName();
-    String getImage();
+import com.cs.jeyz9.condoswiftapi.models.Role;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ShowAllUserDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String lineId;
+    private Set<Role> roles;
+    private BigDecimal creditBalance;
 }
--- src/main/java/com/cs/jeyz9/condoswiftapi/services/UserService.java
+++ src/main/java/com/cs/jeyz9/condoswiftapi/services/UserService.java
@@ -5,11 +5,13 @@ import com.cs.jeyz9.condoswiftapi.dto.RecommendedAgenDTO;
 import com.cs.jeyz9.condoswiftapi.dto.ShowAllAnnounceDetailsWithAgent;
 import com.cs.jeyz9.condoswiftapi.dto.ShowAllUserDTO;
 import com.cs.jeyz9.condoswiftapi.dto.ShowUserDetailsDTO;
+import com.cs.jeyz9.condoswiftapi.dto.TableResponse;
 import com.cs.jeyz9.condoswiftapi.dto.UserProfileOverviewDTO;
 import com.cs.jeyz9.condoswiftapi.exceptions.WebException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface UserService {
@@ -23,5 +25,5 @@ public interface UserService {
     List<ShowAllAnnounceDetailsWithAgent> showAllAnnounceBookmark(String email);
     String updateUserProfile(String email, EditProfileDTO editProfile);
     ShowUserDetailsDTO showUserDetails(String email);
-    List<ShowAllUserDTO> showAllUserSelector();
+    TableResponse<ShowAllUserDTO> showAllUser(String keyword, Integer page, Integer size) throws IOException;
 }
